f6950f39aa42d9f3d38589224942f0a65498264a
“Surajit <surajitkaruri@gmail.com>
2024-01-08 01:17:23 +0530

    Added exception for entity bean field validation

--- src/main/java/com/karurisuro/bookshelf/exception/ControllerExceptionHandler.java
+++ src/main/java/com/karurisuro/bookshelf/exception/ControllerExceptionHandler.java
@@ -4,11 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -27,5 +31,18 @@ public class ControllerExceptionHandler {
     ErrorMessage errorMessage = new ErrorMessage(HttpStatus.BAD_REQUEST, LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
     log.error(errorMessage.toString());
     return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+    Map<String, String> errorMap = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errorMap.put(fieldName, errorMessage);
+      log.error(String.format("Field: %s - Error occurred: %s", errorMessage, fieldName));
+    });
+    ErrorMessage err = new ErrorMessage(HttpStatus.BAD_REQUEST, LocalDateTime.now(), errorMap.toString(), request.getDescription(false));
+    return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
   }
 }
